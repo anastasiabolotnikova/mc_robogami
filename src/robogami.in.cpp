72aca9588182415c7b9e2cb1f4841a67ec129499
anastasiabolotnikova <bolotnikova.anastasia@gmail.com>
2026-06-26 12:51:57 +0200

    Minimal self collisions

--- src/robogami.in.cpp
+++ src/robogami.in.cpp
@@ -37,6 +37,14 @@ RobogamiRobotModule::RobogamiRobotModule() : mc_rbdyn::RobotModule(ROBOGAMI_DESC
   _stance["l1topBase"] = {default_leg_angle};
   _stance["l2TopMove"] = {default_leg_angle};
   _stance["l3TopMove"] = {default_leg_angle};
+
+  // Self-collisions
+  _minimalSelfCollisions = {{"leg1lowerLeftConner", "leg1topLeftConner", 0.01, 0.001, 0.},
+                            {"leg1lowerRightConner", "leg1topRightConner", 0.01, 0.001, 0.},
+                            {"leg2lowerLeftConner", "leg2topLeftConner", 0.01, 0.001, 0.},
+                            {"leg2lowerRightConner", "leg2topRightConner", 0.01, 0.001, 0.},
+                            {"leg3lowerLeftConner", "leg3topLeftConner", 0.01, 0.001, 0.},
+                            {"leg3lowerRightConner", "leg3topRightConner", 0.01, 0.001, 0.}};
 }
 
 }
